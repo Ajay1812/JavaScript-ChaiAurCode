2d3ed79ffe621a6602592a50718c274011ec5685
Ajay1812 <a.kumar01c@gmail.com>
2024-02-08 13:13:29 +0530

    bmi calculator

---
+++ 07_Projects/BMIcalculator/bmiCalculate.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+// console.log(form);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const height = parseInt(document.querySelector(".height").value);
+  const weight = parseInt(document.querySelector(".weight").value);
+  const results = document.querySelector("#results");
+  const bmiguide = document.querySelector("#guide");
+  //   console.log(weight);
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `please give a valid height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `please give a valid weight ${weight}`;
+  } else {
+    let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    console.log(bmi);
+    results.innerHTML = `<span>Your BMI : ${bmi}</span>`;
+    if (bmi < 18.6) {
+      console.log("Under Weight");
+      bmiguide.innerHTML = `<span>BMI: ${bmi}, Your Under Weight.</span>`;
+    } else if (18.6 <= bmi >= 24.9) {
+      console.log("Normal Weight");
+      bmiguide.innerHTML = `<span>BMI: ${bmi}, You are in Normal weight range.</span>`;
+    } else if (bmi > 24.9) {
+      console.log("Over Weight");
+      bmiguide.innerHTML = `<span>BMI: ${bmi}, Your Over Weight.</span>`;
+    }
+  }
+});

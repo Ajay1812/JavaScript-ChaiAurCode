a5949e8054969f3e691966da93716d72de0a70f3
Ajay1812 <a.kumar01c@gmail.com>
2024-02-08 14:31:09 +0530

    another clock

--- 07_Projects/DigitalClock/digitalClock.js
+++ 07_Projects/DigitalClock/digitalClock.js
@@ -1,15 +1,23 @@
-const clock_div = document.querySelector("span");
+const clock_span = document.querySelector("span");
 
-function clock() {
-  const time = new Date().toLocaleString("default", {
+// function clock() {
+//   const time = new Date().toLocaleString("default", {
+//     hourCycle: "h12",
+//     timeStyle: "medium",
+//   });
+//   //   console.clear();
+//   //   console.log(time);
+//   clock_span.innerHTML = time;
+// }
+
+// setInterval(clock, 1000);
+
+// console.log("Hello");
+
+setInterval(() => {
+  let clock = new Date();
+  clock_span.innerHTML = clock.toLocaleTimeString("default", {
     hourCycle: "h12",
     timeStyle: "medium",
   });
-  //   console.clear();
-  //   console.log(time);
-  clock_div.innerHTML = time;
-}
-
-setInterval(clock, 1000);
-
-console.log("Hello");
+}, 1000);

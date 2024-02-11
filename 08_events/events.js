7b501a60e74053961da8d8dfde82df383d32ac1f
Ajay1812 <a.kumar01c@gmail.com>
2024-02-11 15:45:39 +0530

    events and async nature of js

--- 08_events/events.js
+++ 08_events/events.js
@@ -10,18 +10,42 @@
 // clientX , clinetY, screenX, screenY
 // altKey, ctrlKey, shiftKey, keyCode
 
-document.getElementById("images").addEventListener(
-  "click",
-  (e) => {
-    console.log("Click inside ul");
-  },
-  false
-);
+// document.getElementById("images").addEventListener(
+//   "click",
+//   (e) => {
+//     console.log("Click inside ul");
+//   },
+//   true
+// );
 
-document.getElementById("owl").addEventListener(
-  "click",
-  (e) => {
-    console.log("owl");
-  },
-  false
-);
+// document.getElementById("owl").addEventListener(
+//   "click",
+//   (e) => {
+//     console.log("owl");
+//     // e.stopPropagation(); //true
+//   },
+//   true // capturing when its true first it take the outer element than it will dig into elements - > ul->li->img
+// );
+
+// // it is false we called it bubbling, it go from inner element to outer element -> img->li->ul->div
+
+// document.getElementById("google").addEventListener(
+//   "click",
+//   (e) => {
+//     e.preventDefault();
+//     // e.stopPropagation();
+//     console.log("Google Clicked");
+//   },
+//   false
+// );
+document.querySelector("#images").addEventListener("click", (e) => {
+  // console.log(e.target.parentNode); // target -> parent node -> li
+  console.log(e.target.tagName);
+  if (e.target.tagName == "IMG") {
+    console.log(e.target.id);
+    let removeImg = e.target.parentNode;
+    removeImg.remove();
+  } else {
+  }
+  // removeImg.parentNode.removeChild(removeImg);
+});
